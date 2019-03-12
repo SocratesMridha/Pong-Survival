b3934a5909d0a9b4b1713e976c8317e28d5a88e9
SocratesMridha <socratesmridha@gmail.com>
2019-03-12 12:30:27 -0400

    Project Folder

---
+++ Pong Sirvival Project Folder/Unity 2017/Pong Sirvival Android/Assets/Player/Scripts/AccelerometerInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerometerInput : MonoBehaviour {
+
+    [Range(0.1f,1.5f)]public float motionSpeed;
+    public float rangeLimit;
+
+
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Input.acceleration.x * motionSpeed, 0 ,0);
+
+        Vector2 clampedPostion = transform.position;
+
+        clampedPostion.x = Mathf.Clamp(transform.position.x,-rangeLimit,rangeLimit);
+
+        transform.position = clampedPostion;
+        //Debug.Log(Input.acceleration.x * motionSpeed);
+	}
+}
