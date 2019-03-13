8534dd42be37ba561b3bd717ef8f009afb1c406f
Socrates <socratesmridha@gmail.com>
2019-03-12 20:11:33 -0400

    Added a plugin, TextMesh and applied on scenes. Added new scene loader script and title screen to load into game.

---
+++ Pong Sirvival Project Folder/Unity 2017/Pong Sirvival Android/Assets/Managers/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void LoadSceneAsyn (string sceneName)
+    {
+        StartCoroutine(LoadAsynchronously(sceneName));
+    }
+
+    IEnumerator LoadAsynchronously(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+        while(!operation.isDone)
+        {
+            Debug.Log(operation.progress);
+
+            yield return null;
+        }
+    }
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Pong Sirvival Project Folder/Unity 2017/Pong Sirvival Android/Assets/UI/Scrpits/HueController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HueController : MonoBehaviour {
+    public Image bgImage;
+
+    public float fadeSpeed = 15;
+
+    float mHue;
+    float mSaturation;
+    float mValue;
+
+    // Use this for initialization
+    void Start () {
+        //bgImage.GetComponent<Image>();
+        fadeSpeed = 15;
+        mHue = 0.1f;
+        mSaturation = 0.36f;
+        mValue = 1;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        mHue += (Time.deltaTime/fadeSpeed);
+        bgImage.color = Color.HSVToRGB(mHue, mSaturation, mValue );
+
+        if (mHue >=1)
+        {
+            mHue = 0;
+        }
+	}
+}
